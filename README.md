e73dd9c032414a4348a85ffb53c8e49352ac8b0d
getshao <getshao@howtox.com>
2015-12-28 00:23:32 -0800

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# linting with ESLint and Atom
+
+## 需要三个Atom的插件
+- https://atom.io/packages/linter
+- https://github.com/AtomLinter/linter-eslint
+- https://atom.io/packages/react
---
+++ index.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var AppView = React.createClass({
+  displayName: 'AppView',
+  getInitialState: function() {
+    return {};
+  },
+  render: function() {
+    return (<div>test div</div>);
+  }
+});
+
+module.exports = AppView;
